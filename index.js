5f1858500991e48a88b3bc6daaf3a6d455cfc9f1
Rehan Raihan <68391710+raihanuddin2002@users.noreply.github.com>
2021-12-07 20:28:00 +0600

    -

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run () {
         })
         app.get("/wordInfoLimit", async (req,res) => {
             const query = {page:"home"};
-            const cursor = wordsCollention.find(query).limit(60).sort( {_id: ObjectId(-1)});
+            const cursor = wordsCollention.find(query).limit(60).sort( {_id: -1});
             const result =await cursor.toArray();
             res.send(result);
         })
