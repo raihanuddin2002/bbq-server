250f1e4f20c8e506fcbe24504a42211774dc31a3
Rehan Raihan <68391710+raihanuddin2002@users.noreply.github.com>
2021-12-11 23:55:39 +0600

    login

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const env = require('dotenv').config();
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
@@ -17,8 +17,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.svqjf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run () {
-    try{
+async function run() {
+    try {
         /*==================================
           *       Words Collection start
           *==================================*/
@@ -27,79 +27,79 @@ async function run () {
         const wordsCollention = database.collection("wordCollection");
 
         // GET API
-        app.get("/wordInfo", async (req,res) => {
-            const cursor = wordsCollention.find({}).sort( {_id: -1});
-            const result =await cursor.toArray();
+        app.get("/wordInfo", async (req, res) => {
+            const cursor = wordsCollention.find({}).sort({ _id: -1 });
+            const result = await cursor.toArray();
             res.send(result);
         });
-        app.get("/wordInfo/:id", async (req,res) => {
+        app.get("/wordInfo/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
-            const result =await wordsCollention.findOne(query);
+            const query = { _id: ObjectId(id) }
+            const result = await wordsCollention.findOne(query);
             res.send(result);
         });
-        app.get("/wordInfoLimit", async (req,res) => {
-            const query = {page:"home"};
-            const cursor = wordsCollention.find(query).limit(60).sort( {_id: -1});
-            const result =await cursor.toArray();
+        app.get("/wordInfoLimit", async (req, res) => {
+            const query = { page: "home" };
+            const cursor = wordsCollention.find(query).limit(60).sort({ _id: -1 });
+            const result = await cursor.toArray();
             res.send(result);
         });
-        app.get("/wordJungUrl", async (req,res) => {
-            const cursor = wordsCollention.find({}).sort( {_id: -1});
-            const result =await cursor.toArray();
+        app.get("/wordJungUrl", async (req, res) => {
+            const cursor = wordsCollention.find({}).sort({ _id: -1 });
+            const result = await cursor.toArray();
             res.send(result);
         });
 
         // POST API
-        app.post("/addword", async (req,res) => {
+        app.post("/addword", async (req, res) => {
             const wordInfo = req.body;
             const result = await wordsCollention.insertOne(wordInfo);
             res.send(result);
         });
 
-        app.post("/wordJungUrlSearch", async (req,res) => {
+        app.post("/wordJungUrlSearch", async (req, res) => {
             const searchText = req.body.searchText;
-            const query = {wordLine1: { $regex: searchText, $options: 'si'}}; //{wordLine1: { $regex: searchText, $options: 'si'}}
-            const cursor = wordsCollention.find(query).sort( {_id: -1});
-            const result =await cursor.toArray();
+            const query = { wordLine1: { $regex: searchText, $options: 'si' } }; //{wordLine1: { $regex: searchText, $options: 'si'}}
+            const cursor = wordsCollention.find(query).sort({ _id: -1 });
+            const result = await cursor.toArray();
             res.send(result);
         });
-        app.post("/word2JungUrlSearch", async (req,res) => {
+        app.post("/word2JungUrlSearch", async (req, res) => {
             const searchText = req.body.searchText2;
-            const query = {wordLine2: { $regex: searchText, $options: 'si'}}; //{wordLine1: { $regex: searchText, $options: 'si'}}
-            const cursor = wordsCollention.find(query).sort( {_id: -1});
-            const result =await cursor.toArray();
+            const query = { wordLine2: { $regex: searchText, $options: 'si' } }; //{wordLine1: { $regex: searchText, $options: 'si'}}
+            const cursor = wordsCollention.find(query).sort({ _id: -1 });
+            const result = await cursor.toArray();
             res.send(result);
         });
-        app.post("/userJungUrlSearch", async (req,res) => {
+        app.post("/userJungUrlSearch", async (req, res) => {
             const searchUser = req.body.searchUser;
-            const query = {username:{ $regex: searchUser, $options: 'si'}};
-            const cursor = wordsCollention.find(query).sort( {_id: -1});
-            const result =await cursor.toArray();
+            const query = { username: { $regex: searchUser, $options: 'si' } };
+            const cursor = wordsCollention.find(query).sort({ _id: -1 });
+            const result = await cursor.toArray();
             res.send(result);
         });
 
         // PUT API
-        app.put("/updateWordInfo/:id", async (req,res) => {
+        app.put("/updateWordInfo/:id", async (req, res) => {
             const id = req.params.id;
             const updateWordInfo = req.body;
-            const filter = {_id: ObjectId(id) };
+            const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updateDoc = {
                 $set: updateWordInfo
-              };
+            };
             const result = await wordsCollention.updateOne(filter, updateDoc, options);
             res.send(result);
         });
         // DELETE API
-        app.delete("/wordDelete/:id",async (req,res) => {
+        app.delete("/wordDelete/:id", async (req, res) => {
             const id = req.params.id;
             console.log(id)
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await wordsCollention.deleteOne(query);
             res.send(result);
         });
-         // Words Collection end
+        // Words Collection end
 
         /*==================================
         *       Users Collection start
@@ -108,81 +108,100 @@ async function run () {
 
         // GET API
         let userData;
-        app.get("/verifyEmail", async(req,res) => {
-            try{
+        app.get("/verifyEmail", async (req, res) => {
+            try {
                 let count = 1;
-                const {firstName,lastName,country,email,password,isVerified} = userData;
+                const { firstName, lastName, country, email, password, isVerified } = userData;
                 const token = await req.query.token;
 
-                if (token === email && count === 1){
+                if (token === email && count === 1) {
                     count = count + 1;
 
-                    const hashPassword = await bcrypt.hash(password,10);
-                    const saveUserInfo ={
-                        firstName,lastName,country,email,password:hashPassword,isVerified:true
+                    const hashPassword = await bcrypt.hash(password, 10);
+                    const saveUserInfo = {
+                        firstName, lastName, country, email, password: hashPassword, isVerified: true
                     }
-                    
+
                     const result = await usersCollention.insertOne(saveUserInfo);
-                    
+
                     // res.redirect("https://bbqq-e532b.web.app/verifyEmail?emailstatus=verified");
                     res.send("Email Verified Successfully!");
-                    
 
-                } 
-            }catch(err) {
+
+                }
+            } catch (err) {
                 res.status(400).json({
                     message: 'Invalid Token!!'
-                 });
+                });
             }
         });
 
-        //   POST API
-        app.post("/addUser", async (req,res) => {
-            const userInfo = req.body.userInfo;
-            const {firstName,lastName,country,email,password,isVerified} = userInfo;
 
-            const isUserExist = await usersCollention.findOne({email});
-            if(isUserExist){
+        //   POST API
+        app.post("/addUser", async (req, res) => {
+            const userInfo = req.body.userInfo;
+            const { firstName, lastName, country, email, password, isVerified } = userInfo;
+
+            const isUserExist = await usersCollention.findOne({ email });
+            if (isUserExist) {
                 return res.status(400).json({
                     message: 'User Already Exist!'
-                 });
-            }else{
+                });
+            } else {
                 // Mail send
-                    const senderInfo = nodemailer.createTransport({
-                        service:"gmail",
-                        auth:{
-                            user:"jungurl2021@gmail.com",
-                            pass:"Jung1820@"
-                        },
-                        tls:{
-                            rejectUnauthorized:false
-                        }
-                    })
+                const senderInfo = nodemailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: "jungurl2021@gmail.com",
+                        pass: "Jung1820@"
+                    },
+                    tls: {
+                        rejectUnauthorized: false
+                    }
+                })
                 // Send Email to user
                 const mailOptions = {
-                    from:`Verify Your Email <jungurl@gmail.com>`,
-                    to:email,
+                    from: `Verify Your Email <jungurl2021@gmail.com>`,
+                    to: email,
                     subject: "Verified Email check",
-                    html:`<h2>Thank you for registering our website Jungurl</h2> <h4>Please Verify Your Email...</h4> <a href="https://bbq-server.herokuapp.com/verifyEmail?token=${email}">Click Here to verify</a>`
+                    html: `<h2>Thank you for registering our website Jungurl</h2> <h4>Please Verify Your Email...</h4> <a href="https://bbq-server.herokuapp.com/verifyEmail?token=${email}">Click Here to verify</a>`
                 }
 
                 // Sending Mail
-                senderInfo.sendMail(mailOptions, function(err, info){
-                    if(err){
+                senderInfo.sendMail(mailOptions, function (err, info) {
+                    if (err) {
                         return res.status(400).json({
                             message: 'Try Again Something went Wrong!!'
-                         });
-                    }else{
-                        userData = {firstName,lastName,country,email,password,isVerified};
+                        });
+                    } else {
+                        userData = { firstName, lastName, country, email, password, isVerified };
                         res.send("Verification link sent! Check your gmail :)");
                     }
                 })
             }
+        });
+        // Login
+        app.post("/login", async function (req, res) {
+            try {
+                const loginInfo = req.body.loginInfo;
+                const result = await usersCollention.findOne({ email: loginInfo.email });
+                const isMatch = await bcrypt.compare(loginInfo.password, result.password);
 
-
+                if (!isMatch) {
+                    res.status(400).json({
+                        message: "Username or Password is Incorrect!"
+                    })
+                } else {
+                    res.send("Login Successfully!");
+                }
+            } catch (err) {
+                res.status(400).json({
+                    message: "Username or Password is Incorrect!"
+                })
+            }
         });
 
-    }finally{
+    } finally {
 
     }
 
@@ -191,7 +210,7 @@ run().catch(console.dir());
 
 
 // Basic
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("JungUrl server Server Running...");
 });
 
